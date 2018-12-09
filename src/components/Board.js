4a2696e74b0fd373841c0697c8e92550a1478ec7
Steedance <andy.m.stevens@hotmail.co.uk>
2018-12-09 21:48:10 +0000

    look at App.js, I want the function change() to execute when I click on button "a".

---
+++ src/components/Board.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const style = {
+    width: "150px",
+    height: "150px",
+    backgroundColor: "blue",
+    float: "left",
+  }
+
+  
+
+  const Board = () => (
+    <div>
+      {/* <button id="a" style={style}>a</button>
+      <button id="b" style={style}>b</button>
+      <button id="c" style={style}>c</button>
+      <button id="d" style={style}>d</button>
+      <button id="e" style={style}>e</button>
+      <button id="f" style={style}>f</button>
+      <button id="g" style={style}>g</button>
+      <button id="h" style={style}>h</button>
+      <button id="i" style={style}>i</button> */}
+      <button style={style}>!!!</button>
+    </div>
+  )
+
+  export default Board
