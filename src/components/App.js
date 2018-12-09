fdda56b7d536f5859e296e77094f13fc86427569
Steedance <andy.m.stevens@hotmail.co.uk>
2018-12-09 23:29:43 +0000

    Cleaned up directory. (deleted unused js files.) Added 'turns' via the variable "alternator".

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,7 @@
 import React from 'react'
-import Header from './Header'
-import Main from './Main'
 import Board from './Board'
-import Dom from 'react-dom'
-//import naught from './public/naught.png'
+//import Naught from '../../public/naught.png'
+import Cross from '@material-ui/icons/Clear'
 
 const style = {
   width: "150px",
@@ -12,21 +10,30 @@ const style = {
   float: "left",
 }
 
+var alternator = true
+
 function change(element) {
   var a = document.getElementById(element);
-  a.style.backgroundColor = "red"
-  a.style.backgroundImage = "%PUBLIC_URL%/naught.png"
+  if (alternator === true) {
+    a.style.backgroundImage = "url(./cross.png)"
+    a.style.backgroundSize = "cover"
+    alternator = false
+  } else {
+    a.style.backgroundImage = "url(./naught.png)"
+    a.style.backgroundSize = "cover"
+    alternator = true
+  }
+   
+  
   console.log('hell')
 }
 
 const App = () => (
 
   <div>
-    <Header />
-    <Main />
+    <Cross/>
     <div  id="layout" name="andy" style={{maxWidth: "450px", maxHeight: "450px", float: "left", backgroundColor: "Green"}}>LAYOUT
       <div>
-        {/* <button id="a" onClick={() => change()} style={style}>a</button> */}
         <div id="a" onClick={() => change("a")} style={style}>a</div>
         <div id="b" onClick={() => change("b")} style={style}>b</div>
         <div id="c" onClick={() => change("c")} style={style}>c</div>
