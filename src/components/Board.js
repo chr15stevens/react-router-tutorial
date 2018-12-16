0d65f650c87916a0cda67e8085c9b6536c76533e
Steedance <andy.m.stevens@hotmail.co.uk>
2018-12-16 21:10:15 +0000

    Restructuring.

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,50 +1,82 @@
 import React from 'react'
-import Slot from './Slot'
+//import Slot from './Slot'
+
+const style = {
+  width: "150px",
+  height: "150px",
+  backgroundColor: "FF0000",
+  float: "left",
+  border: "1px solid black",
+}
+
+const imagesPath = {
+  empty: "./empty.svg",
+  cross: "./cross.svg",
+  naught: "./naught.svg",
+}
+
+var alternator = false
 
 class Board extends React.Component {
     constructor(props) {
         super(props)
-        this.e = null
-        this.slots = [
-            <Slot key={1} callback={recieve}/>,
-            <Slot key={2}/>,
-            <Slot key={3}/>,
-            <Slot key={4}/>,
-            <Slot key={5}/>,
-            <Slot key={6}/>,
-            <Slot key={7}/>,
-            <Slot key={8}/>,
-            <Slot key={9}/>
-        ]
-        this.recieve = this.recieve.bind(this)
+
+        this.state = {
+            Grid: [],
+            Image: imagesPath.empty,
+            isNaught: null
+        }
+
+        this.click = this.click.bind(this)
         this.track = []
-        this.winCheck = this.winCheck.bind(this)
-    }
-    
-    recieve() {
-        //what i need from child (this.state.isNaught)
+        this.listSlots = this.listSlots.bind(this)
+        this.Slot = this.Slot.bind(this)
     }
 
-    winCheck = () => {
-        this.track[1] = this.slots[1].props.hi
-        //console.log(this.track[1])
-    }
+    click(props) {
+          if (this.state.Image === imagesPath.empty) {
+          if (alternator === true) {
+            this.setState({
+              Image: imagesPath.naught,
+              isNaught: true
+            })
+            alternator = false
+          } else {
+            this.setState({
+              Image: imagesPath.cross,
+              isNaught: false
+            })
+            alternator = true
+            }
+            console.log(this.state.Image)
+          }
+        }
     
 
+    Slot(props) {
+        return <img style={props.style} src={props.src} alt="sorry" onClick={this.click}></img>
+    }
+
+    listSlots() {
+        const lol = [
+            <this.Slot key={1} style={style} src={this.state.Grid[1].Image}/>,
+            <this.Slot key={2} style={style} src={this.state.Grid[2].Image}/>,
+            <this.Slot key={3} style={style} src={this.state.Grid[3].Image}/>,
+            <this.Slot key={4} style={style} src={this.state.Grid[4].Image}/>,
+            <this.Slot key={5} style={style} src={this.state.Grid[5].Image}/>,
+            <this.Slot key={6} style={style} src={this.state.Grid[6].Image}/>,
+            <this.Slot key={7} style={style} src={this.state.Grid[7].Image}/>,
+            <this.Slot key={8} style={style} src={this.state.Grid[8].Image}/>,
+            <this.Slot key={9} style={style} src={this.state.Grid[9].Image}/>
+        ]
+        return lol
+    }
+    
     render() {
+
        return( 
         <div>
-            {this.slots}
-            {this.winCheck()}
-            {/* <Slot id="1"/>
-            <Slot id="2"/>
-            <Slot id="3"/>
-            <Slot id="4"/>
-            <Slot id="5"/>
-            <Slot id="6"/>
-            <Slot id="7"/>
-            <Slot id="8"/>
-            <Slot id="9"/> */}
+            {this.listSlots()}
         </div>
         )
     }
--- src/components/Slot.js
+++ src/components/Slot.js
@@ -1,63 +1,40 @@
 import React from 'react'
 
-const style = {
-  width: "150px",
-  height: "150px",
-  backgroundColor: "FF0000",
-  float: "left",
-  border: "1px solid black",
-  }
 
-  const imagesPath = {
-    empty: "./empty.svg",
-    cross: "./cross.svg",
-    naught: "./naught.svg",
-  }
 
   var alternator = true
 
   class Slot extends React.Component {
     constructor(props) {
       super(props)
-      this.state = {Image: imagesPath.empty, isNaught: null}
-      this.click = this.click.bind(this)
-      this.grid = this.grid.bind(this)
-      this.send = this.send.bind(this)
-    }
-    
-    send(val) {
-      console.log(val)
+      // this.state = {Image: imagesPath.empty, isNaught: null}
+      // this.click = this.click.bind(this)
+      // this.send = this.send.bind(this)
     }
 
-    grid() {
-      return (
-          <img style={style} src={this.state.Image} alt="sorry" onClick={this.click} onChange={this.send(this.state.isNaught)}></img>
-      )
-    }
-
-    click(props) {
-      if (this.state.Image === imagesPath.empty) {
-      if (alternator === true) {
-        this.setState({
-          Image: imagesPath.naught,
-          isNaught: true
-        })
-        alternator = false
-        //if(this.state.isNaught != null) this.send(this.state.isNaught)
-      } else {
-        this.setState({
-          Image: imagesPath.cross,
-          isNaught: false
-        })
-        alternator = true
-        //this.send(this.state.isNaught)
-        }
-      //console.log(alternator)
-      }
-    }
+    // click(props) {
+    //   if (this.state.Image === imagesPath.empty) {
+    //   if (alternator === true) {
+    //     this.setState({
+    //       Image: imagesPath.naught,
+    //       isNaught: true
+    //     })
+    //     alternator = false
+    //     //if(this.state.isNaught != null) this.send(this.state.isNaught)
+    //   } else {
+    //     this.setState({
+    //       Image: imagesPath.cross,
+    //       isNaught: false
+    //     })
+    //     alternator = true
+    //     //this.send(this.state.isNaught)
+    //     }
+    //   //console.log(alternator)
+    //   }
+    // }
     
     render() {
-      return this.grid()        
+      return <img style={style} src={src} alt="sorry"></img>        
     }
   }
 
